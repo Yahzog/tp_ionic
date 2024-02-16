eedbf7d17f3c0f1ef8605e0f2c4b12ec88360143
Yahzog <engelmann.quentin@gmail.com>
2024-02-16 10:45:34 +0100

    environment prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  firebase: {
+    apiKey: "AIzaSyB3VrDlp2UQNWP_7J2rTq26LPSq1SUampk",
+    authDomain: "dark-souls-app.firebaseapp.com",
+    projectId: "dark-souls-app",
+    storageBucket: "dark-souls-app.appspot.com",
+    messagingSenderId: "315861698135",
+    appId: "1:315861698135:web:e39ece019dd6fe07cbc512"
+  },
+  production: false
 };
+
